bec7f4f9e0bf8fc1aad65a805a6330d62e6cf192
Anthony Quizon <anthoq88@gmail.com>
2025-10-20 10:40:53 +1100

    rename

--- dbq.sh
+++ dbq.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Allows symlink or nonsymlink versions of script. eg. if calling from symlink file in ~/.bin
 src="$(dirname "$(readlink -f "$0")")"
-bqn "$src/qbqn" "$@"
+bqn "$src/dbq" "$@"
